32404e6c2f62acea383e704fd04a8e300b73edc2
Ruth <ruthp8798@gmail.com>
2024-09-17 21:55:31 +0300

    adding files

---
+++ 01_first_run.py
@@ -0,0 +1,30 @@
+# print(3*4)
+# print(3**4)
+# print(3/4)
+# print(3//4)
+# base_url = "http://www.google.co.il"
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+# print(base_url)
+###
+# name = "ruth"
+# age = 24
+# print(name)
+# print(age)
+# print(name + " " + str(age))
+# print(f"{name} {age}")
+###
+# name = input("enter your name: ")
+# print(name)
+###
+# num1 = input("press num1: ")
+# num2 = input("press num2: ")
+# print(num1+num2)  # 510- input by default str
+# print(int(num1)+int(num2))
+# print(type(num1))
+# print(type(num1))
+# print(f"{int(num1) + int(num2)}")
+# print("hellow world")
